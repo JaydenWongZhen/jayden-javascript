1ca2b90dfc676442cc83161c639c3a67471b1390
JaydenWongZhen <taymeister@gmail.com>
2025-07-14 10:16:55 +0000

    Jul 14, 2025, 10:16 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -9,7 +9,7 @@ function setup() {
 
 function draw() {
   // Challenge 1: An eye using ellipse() function
-rect(300,200,100,10)
+rect(300,200,10,100)
 fill(255,255,255);
 ellipse(300,200,200,150)
 fill(0,0,0);
