2923e0c703adcac8b22069d21c35284c0425f197
JaydenWongZhen <taymeister@gmail.com>
2025-07-14 10:31:15 +0000

    Jul 14, 2025, 10:31 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -32,7 +32,7 @@ function draw() {
   // Challenge 3: A house using rect() function
   fill(255,200,150)
   rect(300,200,250,200)
-  rect(300,200,50,75)
+  rect(50,200,50,75)
 
   // Challenge 4: A rocketship using triangle() function
 
