ac91089556440d50ab7c1e94723645f0ebe39a4e
JaydenWongZhen <taymeister@gmail.com>
2025-07-14 10:39:42 +0000

    Jul 14, 2025, 10:39 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -36,7 +36,7 @@ function draw() {
   rect(400,300,75,100)
   fill(0,150,255)
   rect(300,300,75,75)
-  
+  fill
   triangle(450,150,300,250,550,250)
 
   // Challenge 4: A rocketship using triangle() function
