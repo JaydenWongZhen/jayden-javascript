78c44d4419261bcfda680ddcf2463cb1ef0d9061
JaydenWongZhen <taymeister@gmail.com>
2025-07-14 10:05:03 +0000

    Jul 14, 2025, 10:05 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -9,7 +9,7 @@ function setup() {
 
 function draw() {
   // Challenge 1: An eye using ellipse() function
-
+CSSNumericValue(300)
   // Challenge 2: A face using circle() function
 
   // Challenge 3: A house using rect() function
