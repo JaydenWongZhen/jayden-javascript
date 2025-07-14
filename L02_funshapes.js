7b5c1ea087307bef62ed414f53a4654bd4f3f0d1
JaydenWongZhen <taymeister@gmail.com>
2025-07-14 10:45:47 +0000

    Jul 14, 2025, 10:45 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -40,7 +40,7 @@ function draw() {
   // triangle(450,150,300,250,550,250)
 
   // Challenge 4: A rocketship using triangle() function
-  ellipse(300,200,100,150)
+  ellipse(300,200,100,200)
   triangle(300,50,250,175,350,175)
   circle(300,200,50)
 
