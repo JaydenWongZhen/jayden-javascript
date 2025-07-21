bcb3f0b72c227978c15f9c29f320c436c42a9dae
JaydenWongZhen <taymeister@gmail.com>
2025-07-21 10:11:13 +0000

    Jul 21, 2025, 10:11 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -8,7 +8,8 @@ function draw() {
   // Recap 1: Repeating Circles
 circle(50,150,50);
 circle(125,150,50);
-circle(200,150,50)
+circle(200,150,50);
+circle(275,150,50)
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
