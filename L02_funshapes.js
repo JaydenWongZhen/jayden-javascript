62753c431bb405aad682b00afbf12f9f6a3443fd
JaydenWongZhen <taymeister@gmail.com>
2025-07-21 09:48:23 +0000

    Jul 21, 2025, 9:48 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -55,6 +55,7 @@ function draw() {
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
+  ;no
   fill(255,255,255)
   circle(300,200,200)
 }
