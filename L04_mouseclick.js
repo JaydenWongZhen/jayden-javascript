778d88cdcc5eee3d29aed1870cbecd1ecaee3adb
JaydenWongZhen <taymeister@gmail.com>
2025-07-28 10:31:07 +0000

    Jul 28, 2025, 10:31 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -9,14 +9,14 @@ background(220); //gray
 function draw () {
 let xpos = 60;
 let ypos=180;
-let circle=50;
+let diameter=50;
 let r = 0;
 let g = 0;
 let b = 0;
 for (let i=0; i<5; i++) {
 fill(r,g,b);
 g = g + 50;
-circle(posY,posX,circle);
+circle(posY,posX,diameter);
 xpos = xpos+50;
 ypos = ypos+50;
 }
