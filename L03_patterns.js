9e95319cefb910ae873b1b6c3aeaaaaf2b2cb110
JaydenWongZhen <taymeister@gmail.com>
2025-07-28 09:33:00 +0000

    Jul 28, 2025, 9:33 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -6,20 +6,20 @@ function setup() {
   
 function draw() {
   // Recap 1: Repeating Circles
-let posX = 50;
-let posY = 150;
-let circleD = 50;
-let CB = 0;
+// let posX = 50;
+// let posY = 150;
+// let circleD = 50;
+// let CB = 0;
 
-//le repet
-for (let i=1; i<10; i++) {
-  fill(CB,50,255);
-  CB=CB+25;
-  //le repet
-  circle(posX,posY,circleD)
-  //val renew
-  posX = posX + circleD + 2;
-}
+// //le repet
+// for (let i=1; i<10; i++) {
+//   fill(CB,50,255);
+//   CB=CB+25;
+//   //le repet
+//   circle(posX,posY,circleD)
+//   //val renew
+//   posX = posX + circleD + 2;
+
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
