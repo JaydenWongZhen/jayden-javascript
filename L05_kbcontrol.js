1d3d1e1a29641e2a42280d3689b3e3f5345f53cf
JaydenWongZhen <taymeister@gmail.com>
2025-08-04 10:15:21 +0000

    Aug 4, 2025, 10:15 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -8,12 +8,12 @@ function draw() {
 
 }
 let rectsize=10
-function mousePressed() {
-    fill(random(255),0,random(255));
-    ;noStroke()
-    circlesize=5
-}
-function mouseDragged() {
-    circle(mouseX,mouseY,circlesize);
-    circlesize = circlesize + 0.5;
-}
+// function mousePressed() {
+//     fill(random(255),0,random(255));
+//     ;noStroke()
+//     circlesize=5
+// }
+// function mouseDragged() {
+//     circle(mouseX,mouseY,circlesize);
+//     circlesize = circlesize + 0.5;
+// }
