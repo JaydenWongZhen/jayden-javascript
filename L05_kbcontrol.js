6f92e0e5bffb182133c6428467d8dc77053b24c7
JaydenWongZhen <taymeister@gmail.com>
2025-08-11 09:48:13 +0000

    Aug 11, 2025, 9:48 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -34,7 +34,7 @@ function keyPressed() {
     }
     else if (key === 'b') {
         fill('blue');
-        rect(100,100,)
+        rect(100,100,100,100);
     }
     else {
         fill('white');
