c1800b3ff2051c32a53bbd0ef9448c30da95f27e
JaydenWongZhen <taymeister@gmail.com>
2025-09-05 10:55:41 +0000

    Sep 5, 2025, 10:55 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -22,7 +22,7 @@ function setup() {
     for (let i = 0; i < favfood.length; i++ ){
         // console.log(i);
         // console.log(favfood[i]);
-        text('mi favrity fooods',100,100)
+        text('mi favrity fooods',width/2,)
         text(i+1+"." + favfood[i],width/2,100 + i * 50);
     }
 }
