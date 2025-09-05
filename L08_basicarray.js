2fe7448bada564aa16577faaf7c58caacf10fdd2
JaydenWongZhen <taymeister@gmail.com>
2025-09-05 10:40:36 +0000

    Sep 5, 2025, 10:40 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -19,7 +19,7 @@ function setup() {
     favfood = ['udon noodles','chicken','chips', 'carbonara', 'chicken nuggies', 'drywall'];
     console.log(favfood[2]);
 
-    for (let i = 0; i < 5; i++ ){
+    for (let i = 0; i < favfood.length; i++ ){
         // console.log(i);
         console.log(favfood[i]);
     }
