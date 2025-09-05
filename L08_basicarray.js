72e3f403c17d51f6bc9d3d526edc2fc13140130c
JaydenWongZhen <taymeister@gmail.com>
2025-09-05 10:14:38 +0000

    Sep 5, 2025, 10:14 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -14,7 +14,7 @@ function setup() {
     fill('white');
     textSize(35);
     text("hi guys mi jayden", 50,50);
-    text("im 10 lololol") ,50,100
+    text("im 10 lololol",50,100)
 }
 
 function draw() {
