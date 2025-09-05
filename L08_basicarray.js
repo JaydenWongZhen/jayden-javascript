3cc1116a9c35df70a04298cbae113ef638be4256
JaydenWongZhen <taymeister@gmail.com>
2025-09-05 10:26:04 +0000

    Sep 5, 2025, 10:26 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -16,7 +16,7 @@ function setup() {
     textSize(35);
     textAlign(CENTER,CENTER); 
 
-    favfood = ['udon noodles','chicken','chips',]
+    favfood = ['udon noodles','chicken','chips', '']
 }
 
 function draw() {
