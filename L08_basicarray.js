ad2698de9de51f043b4084dd11caa1d5688bce87
JaydenWongZhen <taymeister@gmail.com>
2025-09-05 09:40:33 +0000

    Sep 5, 2025, 9:40 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,2 +1,11 @@
 // write your codes here
 
+// write your codes here
+let starwars;
+
+function preload() {
+    starwars = 
+}
+function setup() {
+    
+}
--- L08_starwarstext.js
+++
@@ -1,9 +0,0 @@
-// write your codes here
-let starwars;
-
-function preload() {
-    starwars = loa
-}
-function setup() {
-    
-}
