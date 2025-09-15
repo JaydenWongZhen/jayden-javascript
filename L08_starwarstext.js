2837e8c11bcc4be2a9d9e52fc0c22d43248e8d79
JaydenWongZhen <taymeister@gmail.com>
2025-09-15 10:32:01 +0000

    Sep 15, 2025, 10:32 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -30,7 +30,7 @@ let saers = [
     "Both had lived in peace, for a long time,",
     "But on the day of 2019, they had went to war.",
     "One day, a happy child had learned coding,",
-    "and somehow.. he had learnt both "
+    "and somehow.. he had became a MIX OF BOTH RACES."
 
 ]
 text("2lecok",0,0);
