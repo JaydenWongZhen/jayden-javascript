0009e2b33dc1bc201228ce947e07b5ab9815fbe9
JaydenWongZhen <taymeister@gmail.com>
2025-09-15 09:51:15 +0000

    Sep 15, 2025, 9:51 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -8,26 +8,31 @@ let favfood;
 function preload() {
     starwars = loadSound('assets/star_wars_theme_8_bit.mp3');
 }
+
 function setup() {
-    createCanvas(600,400);
-    background(0);
-    booleanflag = false;
-    fill('white');
-    textSize(20);
-    //textAlign(CENTER,CENTER);  [removed code]
-let ypos = 50;
-    favfood = ['udon noodles','chicken','chips', 'carbonara', 'chicken nuggies'];
-    console.log(favfood[2]);
-// text('mi favrite food', width/2,50) [removed code]
-    for (let i = 0; i < favfood.length; i++ ){
-        // console.log(i); [removed code]
-        // console.log(favfood[i]); [removed code]
-        // text(i+1+"." + favfood[i],width/2,100 + i * 50); [removed code]
-        console.log( favfood[i]);
-        text(favfood[i], 20, ypos);
-        ypos = ypos +50;
-    }
+    createCanvas(400,400);
 }
+
+// function setup() {
+//     createCanvas(600,400);
+//     background(0);
+//     booleanflag = false;
+//     fill('white');
+//     textSize(20);
+//     //textAlign(CENTER,CENTER);  [removed code]
+// let ypos = 50;
+//     favfood = ['udon noodles','chicken','chips', 'carbonara', 'chicken nuggies'];
+//     console.log(favfood[2]);
+// // text('mi favrite food', width/2,50) [removed code]
+//     for (let i = 0; i < favfood.length; i++ ){
+//         // console.log(i); [removed code]
+//         // console.log(favfood[i]); [removed code]
+//         // text(i+1+"." + favfood[i],width/2,100 + i * 50); [removed code]
+//         console.log( favfood[i]);
+//         text(favfood[i], 20, ypos);
+//         ypos = ypos +50;
+//     }
+// }
 
 function draw() {
 
