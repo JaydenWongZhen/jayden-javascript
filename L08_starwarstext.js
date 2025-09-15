1ec0d6eb41ea2e652564d585f4a9618cb4b012a9
JaydenWongZhen <taymeister@gmail.com>
2025-09-15 10:45:51 +0000

    Sep 15, 2025, 10:45 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -36,8 +36,8 @@ let saers = [
 ]
 let ytextpos = 0;
 for (let i = 0; i < saers.length; i++ ) {
-text(saers[i],0,);
-text("secjd linn",0,45);
+text(saers[i],0,ytextpos);
+
 ypos = ypos - 0.6;
 ytextpos = ytextpos + 10;
 }
