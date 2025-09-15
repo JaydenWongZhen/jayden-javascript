8488ec22eaaeac453252520ee3f91ec04c54e860
JaydenWongZhen <taymeister@gmail.com>
2025-09-15 10:08:02 +0000

    Sep 15, 2025, 10:08 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,7 +10,9 @@ function preload() {
     starwars = loadSound('assets/star_wars_theme_8_bit.mp3');
 }
 
-
+function setup() {
+    createCanvas(400,400);
+    background(200);
     fill("orange");
     rect(width/2,height/2,200,200);
     fill(0);
--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -3,3 +3,7 @@ let starwars;
 function preload() {
     starwars = loadSound('assets/star_wars_theme_8_bit.mp3');
 }
+
+function setup() {
+    createCanvas(400,400);
+    background(200);
