d5b7673473bd4e027c197cc2417edde4d97da4b9
JaydenWongZhen <taymeister@gmail.com>
2025-09-22 10:04:11 +0000

    Sep 22, 2025, 10:04 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,22 +1,22 @@
-let xpos;
-let xvel = 1;
-function setup() {
-    createCanvas(600,400);
-    background(200);
-xpos = width/2
-}
+// let xpos;
+// let xvel = 1;
+// function setup() {
+//     createCanvas(600,400);
+//     background(200);
+// xpos = width/2
+// }
 
-function draw() {
-     background(200);
-    textSize(25);
-    textAlign(CENTER,CENTER);
-    text("the yoinky sploinky", xpos,height/2);
-    xpos=xpos+xvel;
-    // xvel=xvel*1.01; //experimental code
-  if (xpos > width-200) {
-    xvel=xvel* -1;
-  }
-  else if (xpos < 200) {
-    xvel=xvel*-1;
-  }
-}
+// function draw() {
+//      background(200);
+//     textSize(25);
+//     textAlign(CENTER,CENTER);
+//     text("the yoinky sploinky", xpos,height/2);
+//     xpos=xpos+xvel;
+//     // xvel=xvel*1.01; //experimental code
+//   if (xpos > width-200) {
+//     xvel=xvel* -1;
+//   }
+//   else if (xpos < 200) {
+//     xvel=xvel*-1;
+//   }
+// }
