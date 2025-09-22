49498f9385e98be3b77deec5ebb1ca39c9b69786
JaydenWongZhen <taymeister@gmail.com>
2025-09-22 10:58:09 +0000

    Sep 22, 2025, 10:58 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -45,7 +45,7 @@ text("dude its literally " + hour24 + ":" + min + ":" + nowsecond, width/2,100);
 fill("orange");
 textSize(30);
 text("connt donn", width/2,130);
-text(cd,width/2,150)
+text("testing",width/2,150)
 }
 function mousePressed() {
     homer_simpson.play();
