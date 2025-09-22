7bde771b9e06a45fa1bf32d54afac536e53d0a2f
JaydenWongZhen <taymeister@gmail.com>
2025-09-22 09:58:45 +0000

    Sep 22, 2025, 9:58 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -12,7 +12,7 @@ function draw() {
     textAlign(CENTER,CENTER);
     text("the yoinky sploinky testing v1", xpos,height/2);
     xpos=xpos+xvel;
-    xvel=xvel*1.15;
+    xvel=xvel*1.01;
   if (xpos > width) {
     xvel=xvel* -1;
   }
