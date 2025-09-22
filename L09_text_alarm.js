2b4c5bf26c19f6920dc735596f72c379938a5baa
JaydenWongZhen <taymeister@gmail.com>
2025-09-22 10:54:40 +0000

    Sep 22, 2025, 10:54 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -43,7 +43,7 @@ textSize(50);
 textAlign(CENTER,CENTER);
 text("dude its literally " + hour24 + ":" + min + ":" + nowsecond, width/2,100);
 fill("orange");
-textSize
+textSize(25);
 text("connt donn", width/2,130);
 }
 function mousePressed() {
