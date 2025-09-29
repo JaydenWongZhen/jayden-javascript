f263458f169c4dfb13031f2923eea6c6f94f8552
JaydenWongZhen <taymeister@gmail.com>
2025-09-29 09:40:06 +0000

    Sep 29, 2025, 9:40 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -47,7 +47,7 @@ textSize(30);
 text("connt donn", width/2,130);
 text("0",width/2,175);
 text("start time by clik me", width/2,250);
-
+let pi= 3.1415
 }
 function mousePressed() {
     homer_simpson.play();
