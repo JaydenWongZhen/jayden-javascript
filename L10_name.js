1f7e95aa33f61b85074ab77e388d4ec8bfdbbc85
JaydenWongZhen <taymeister@gmail.com>
2025-10-06 09:59:16 +0000

    Oct 6, 2025, 9:59 AM

--- L10_name.js
+++ L10_name.js
@@ -11,7 +11,7 @@ function setup() {
 
     userInput = createInput(username);
     userInput.position(width/2,height/2 + 100);
-    userInput.input(U)
+    userInput.input(updateName)
 }
 
 function draw() {
