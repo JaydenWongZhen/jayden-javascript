17db9309cf14177a4e5722ee109c64c4f7fded22
JaydenWongZhen <taymeister@gmail.com>
2025-10-06 10:44:59 +0000

    Oct 6, 2025, 10:44 AM

--- L10_name.js
+++ L10_name.js
@@ -44,7 +44,7 @@ textSize(30);
 textAlign(LEFT,CENTER);
 text(username,150,75);
 text(age,150,125);
-text(dob)
+text(dob,150,)
 textAlign(RIGHT,CENTER);
 textSize(20);
 text("change color here", width/2,height/2 + 10);
