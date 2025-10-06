548baaf008a71d0dee3301d748385ee46cea265f
JaydenWongZhen <taymeister@gmail.com>
2025-10-06 09:49:22 +0000

    Oct 6, 2025, 9:49 AM

--- L10_name.js
+++ L10_name.js
@@ -9,7 +9,7 @@ function setup() {
     cp=createColorPicker(bgrd);
     cp.position(width/2,350);
 
-    userInput = createInput("username")
+    userInput = createInput(username)
 }
 
 function draw() {
