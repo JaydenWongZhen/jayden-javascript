5a0db456af229a20dd6141da2dfde19a058c85c8
JaydenWongZhen <taymeister@gmail.com>
2025-10-06 10:59:32 +0000

    Oct 6, 2025, 10:59 AM

--- L10_name.js
+++ L10_name.js
@@ -37,7 +37,7 @@ function updateDob() {
 }
 function draw() {
     background( cp.value());
-    strokeWeight(5);
+    strokeWeight(2.5);
     stroke("blue");
 fill("white");
 rect(100,50,400,150,10);
