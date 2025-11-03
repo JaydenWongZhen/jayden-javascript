5dccbe710a39e7317f82d394595362c39a2626f6
JaydenWongZhen <taymeister@gmail.com>
2025-11-03 10:00:06 +0000

    Nov 3, 2025, 10:00 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,5 +1,5 @@
 // write your codes here
-let todayswordle = ["ligma","balls","s"]
+let todayswordle = ["ligma","balls","sigma"]
 function setup() {
     createCanvas(600,600);
     background(0,150,255);
