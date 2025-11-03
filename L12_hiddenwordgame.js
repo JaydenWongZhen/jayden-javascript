c6656a16993de901e4ab5b7d74fe4eebb5343acf
JaydenWongZhen <taymeister@gmail.com>
2025-11-03 10:51:11 +0000

    Nov 3, 2025, 10:51 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -21,7 +21,7 @@ selected = selected.toUppercase();
 generateHints();
 
 function generateHints() {
-hints = selected[0] + " "
+hints = selected[0] + " " + "_"
 }
 function checkGuess() {
  message = "you are right, it is sigma!"
