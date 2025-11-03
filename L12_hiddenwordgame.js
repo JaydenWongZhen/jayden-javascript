5317d0cddaecf2250b74908909fb93d7b67684d4
JaydenWongZhen <taymeister@gmail.com>
2025-11-03 10:24:01 +0000

    Nov 3, 2025, 10:24 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -10,7 +10,7 @@ ghfgngbyetagain.position(100,100);
 lebuton = createButton("guess")
 lebuton.position(300,100);
 lebuton.size(50,50)
-letbuton.style("","lightblue")
+letbuton.style("backgroundcolor","lightblue")
 
 }
 function draw() {
