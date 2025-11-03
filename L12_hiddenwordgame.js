e9d57c03e0e702c29c68e657469218ad69d0c9e8
JaydenWongZhen <taymeister@gmail.com>
2025-11-03 10:48:48 +0000

    Nov 3, 2025, 10:48 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -17,7 +17,7 @@ lebuton.size(50,50)
 lebuton.style("background-color","blue");
 lebuton.mousePressed(checkGuess);
 selected = random(todayswordle); 
-selected = selected
+selected = selected.toUpper
 
 function checkGuess() {
  message = "you are right, it is sigma!"
